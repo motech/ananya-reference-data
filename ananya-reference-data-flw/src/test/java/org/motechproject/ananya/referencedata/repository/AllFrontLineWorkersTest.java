8568986e77c8abb95c723a5bf5f4b56a25f9a8c1
Imdad Ahmed <imdadah@thoughtworks.com>
2012-06-04 12:14:29 +0530

    Imdad, Sharvani | #00 | Refactored code to merge domain and service modules into one single module: ananya-reference-data-flw

---
+++ ananya-reference-data-flw/src/test/java/org/motechproject/ananya/referencedata/repository/AllFrontLineWorkersTest.java
@@ -0,0 +1,84 @@
+package org.motechproject.ananya.referencedata.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.motechproject.ananya.referencedata.domain.FrontLineWorker;
+import org.motechproject.ananya.referencedata.domain.Location;
+import org.motechproject.ananya.referencedata.SpringIntegrationTest;
+import org.motechproject.ananya.referencedata.domain.Designation;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class AllFrontLineWorkersTest extends SpringIntegrationTest{
+
+    @Autowired
+    AllFrontLineWorkers allFrontLineWorkers;
+
+    @Before
+    @After
+    public void setUp() {
+        template.deleteAll(template.loadAll(FrontLineWorker.class));
+        template.deleteAll(template.loadAll(Location.class));
+    }
+
+    @Test
+    public void shouldAddFLWToDB() {
+        Location location = new Location("district", "block", "panchayat");
+        FrontLineWorker frontLineWorker = new FrontLineWorker(1234567890L, "name", Designation.ANGANWADI, location);
+
+        allFrontLineWorkers.add(frontLineWorker);
+
+        List<FrontLineWorker> frontLineWorkerList = template.loadAll(FrontLineWorker.class);
+        assertEquals(1, frontLineWorkerList.size());
+    }
+
+    @Test
+    public void shouldUpdateFLWToDB() {
+        long msisdn = 1234567890L;
+        FrontLineWorker existingFrontLineWorker = new FrontLineWorker(msisdn, "name", Designation.ANGANWADI, new Location("district", "block", "panchayat"));
+        allFrontLineWorkers.add(existingFrontLineWorker);
+
+        String newName = "new_name";
+        Designation newDesignation = Designation.ANM;
+        String newDistrict = "district1";
+        existingFrontLineWorker.setName(newName);
+        existingFrontLineWorker.setDesignation(newDesignation);
+        existingFrontLineWorker.setLocation(new Location(newDistrict, "block1", "panchayat1"));
+        allFrontLineWorkers.update(existingFrontLineWorker);
+
+        FrontLineWorker frontLineWorkerFromDb = allFrontLineWorkers.getFor(msisdn);
+        assertEquals(newName, frontLineWorkerFromDb.getName());
+        assertEquals(newDesignation.name(), frontLineWorkerFromDb.getDesignation());
+        assertEquals(newDistrict, frontLineWorkerFromDb.getLocation().getDistrict());
+    }
+
+    @Test
+    public void shouldGetAllFLWsFromDB() {
+        Location location1 = new Location("district", "block", "panchayat");
+        FrontLineWorker frontLineWorker1 = new FrontLineWorker(1234567890L, "name", Designation.ANGANWADI, location1);
+        Location location2 = new Location("district", "block", "panchayat");
+        FrontLineWorker frontLineWorker2 = new FrontLineWorker(1234567890L, "name", Designation.ANGANWADI, location2);
+
+        allFrontLineWorkers.add(frontLineWorker1);
+        allFrontLineWorkers.add(frontLineWorker2);
+
+        List<FrontLineWorker> frontLineWorkerList = allFrontLineWorkers.getAll();
+        assertEquals(2, frontLineWorkerList.size());
+    }
+
+    @Test
+    public void shouldGetAnFLWForTheGivenMsisdn() {
+        Location location = new Location("district", "block", "panchayat");
+        Long msisdn = 1234567890L;
+        FrontLineWorker frontLineWorker = new FrontLineWorker(msisdn, "name", Designation.ANGANWADI, location);
+        allFrontLineWorkers.add(frontLineWorker);
+
+        FrontLineWorker frontLineWorkerFromDB = allFrontLineWorkers.getFor(msisdn);
+
+        assertEquals("name", frontLineWorkerFromDB.getName());
+    }
+}
---
+++ ananya-reference-data-flw/src/test/java/org/motechproject/ananya/referencedata/repository/AllLocationsTest.java
@@ -0,0 +1,72 @@
+package org.motechproject.ananya.referencedata.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.motechproject.ananya.referencedata.domain.Location;
+import org.motechproject.ananya.referencedata.SpringIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+public class AllLocationsTest extends SpringIntegrationTest {
+
+    @Autowired
+    AllLocations allLocations;
+
+    @Before
+    @After
+    public void setUp() {
+        template.deleteAll(template.loadAll(Location.class));
+    }
+
+    @Test
+    public void shouldAddLocationToDB() {
+        Location location = new Location("district", "block", "panchayat");
+
+        allLocations.add(location);
+
+        List<Location> locations = template.loadAll(Location.class);
+        assertEquals(1, locations.size());
+        assertTrue(location.equals(locations.get(0)));
+    }
+
+    @Test
+    public void shouldReturnAllLocationsFromDB() {
+        Location location1 = new Location("district", "block", "panchayat");
+        Location location2 = new Location("district", "block", "panchayat");
+
+        allLocations.add(location1);
+        allLocations.add(location2);
+
+        List<Location> locations = allLocations.getAll();
+
+        assertEquals(2, locations.size());
+    }
+
+    @Test
+    public void shouldReturnLocationWhenLocationForGivenLocationDetailsExists() {
+        String district = "district";
+        String block = "block";
+        String panchayat = "panchayat";
+        Location location = new Location(district, block, panchayat);
+
+        allLocations.add(location);
+
+        Location locationFromDB = allLocations.getFor(district, block, panchayat);
+        assertNotNull(locationFromDB);
+    }
+
+    @Test
+    public void shouldReturnNullWhenALocationDoesNotExistInDB(){
+        String district = "district";
+        String block = "block";
+        String panchayat = "panchayat";
+
+        Location locationInDB = allLocations.getFor(district, block, panchayat);
+
+        assertNull(locationInDB);
+    }
+}
