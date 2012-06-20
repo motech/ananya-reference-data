014226abc84bca5ed8c4fe6ead38bcf58091439f
Aravind M <arav.psgtech@gmail.com>
2012-06-20 18:16:55 +0530

    Aravind/Sharvani | #255 | Making the cron expression configurable.

--- ananya-reference-data-flw/src/main/java/org/motechproject/ananya/referencedata/flw/repository/AllSyncJobs.java
+++ ananya-reference-data-flw/src/main/java/org/motechproject/ananya/referencedata/flw/repository/AllSyncJobs.java
@@ -5,16 +5,22 @@ import org.motechproject.scheduler.MotechSchedulerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Properties;
+
 @Component
 public class AllSyncJobs {
     private MotechSchedulerService schedulerService;
+    private Properties referenceDataProperties;
+    private final String CRON_EXPRESSION_KEY = "scheduler.cron.expression";
 
     @Autowired
-    public AllSyncJobs(MotechSchedulerService schedulerService) {
+    public AllSyncJobs(MotechSchedulerService schedulerService, Properties referenceDataProperties) {
         this.schedulerService = schedulerService;
+        this.referenceDataProperties = referenceDataProperties;
     }
 
     public void addFrontLineWorkerSyncJob() {
-        schedulerService.safeScheduleJob(new FrontLineWorkerSyncJob());
+        String cronExpression = (String) referenceDataProperties.get(CRON_EXPRESSION_KEY);
+        schedulerService.safeScheduleJob(new FrontLineWorkerSyncJob(cronExpression));
     }
 }
