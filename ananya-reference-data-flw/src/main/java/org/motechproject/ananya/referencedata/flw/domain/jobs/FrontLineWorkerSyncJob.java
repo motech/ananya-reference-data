36ed6ed451a0e114b51492e750a4b6f9cb74d02e
Aravind M <arav.psgtech@gmail.com>
2012-06-21 12:04:10 +0530

    Revert "Aravind/Sharvani | #255 | Making the cron expression configurable."
    This reverts commit 014226abc84bca5ed8c4fe6ead38bcf58091439f.

--- ananya-reference-data-flw/src/main/java/org/motechproject/ananya/referencedata/flw/domain/jobs/FrontLineWorkerSyncJob.java
+++ ananya-reference-data-flw/src/main/java/org/motechproject/ananya/referencedata/flw/domain/jobs/FrontLineWorkerSyncJob.java
@@ -5,7 +5,7 @@ import org.motechproject.model.CronSchedulableJob;
 import org.motechproject.model.MotechEvent;
 
 public class FrontLineWorkerSyncJob extends CronSchedulableJob {
-    public FrontLineWorkerSyncJob(String cronExpression) {
-        super(new MotechEvent(SyncEventKeys.SCHEDULE_SYNC), cronExpression);
+    public FrontLineWorkerSyncJob() {
+        super(new MotechEvent(SyncEventKeys.SCHEDULE_SYNC), "0 */15 * * * ?");
     }
 }
