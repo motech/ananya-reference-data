c87e3bc1a01145be6c90e0dbb042e6d4c31feac9
f1codz <imdad.ahmed@gmail.com>
2012-11-16 10:53:47 +0530

    Vishnu, Imdad | #115 | Configuring spring default authentication provider based security

--- ananya-reference-data-admin/src/main/java/org/motechproject/ananya/referencedata/admin/security/AuthenticationEntryPoint.java
+++
@@ -1,6 +0,0 @@
-package org.motechproject.ananya.referencedata.admin.security;
-
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-
-public class AuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint {
-}
